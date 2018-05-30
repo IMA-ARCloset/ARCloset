ac9d5500402ede397861c850f5b2a9db4924e653
CarlosMolins <carlesmolins13@gmail.com>
2018-05-30 11:20:50 +0200

    anyadido egypt effect a egyptController, borrado de carpetas y escenas

--- Assets/Final_Project/Scripts/EgyptController.cs
+++ Assets/Final_Project/Scripts/EgyptController.cs
@@ -2,31 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EgyptController : MonoBehaviour {
+public class EgyptController : MonoBehaviour
+{
 
     public Manager manager;
     public GameObject[] l_EgyptTorchs;
+    public GameObject[] l_waterfalls;
 
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         l_EgyptTorchs = GameObject.FindGameObjectsWithTag("EgyptTorch");
+
+        l_waterfalls = GameObject.FindGameObjectsWithTag("Waterfalls");
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (manager.day)
             foreach (GameObject gO in l_EgyptTorchs)
             {
                 gO.GetComponentInChildren<ParticleSystem>().Stop(true, ParticleSystemStopBehavior.StopEmitting);
             }
+
+        foreach (GameObject gO in l_waterfalls)
+        {
+            var l_aux = gO.GetComponentsInChildren<ParticleSystem>();
+            foreach (ParticleSystem p in l_aux)
+                p.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
     }
 
-    public void manageTorchs()
-    {
-        StartCoroutine(Manage_EgyptTorchs());
-    }
-
-    IEnumerator Manage_EgyptTorchs()
+    public IEnumerator Manage_EgyptTorchs()
     {
 
         if (manager.day)
@@ -34,7 +42,7 @@ public class EgyptController : MonoBehaviour {
             foreach (GameObject t in l_EgyptTorchs)
             {
                 t.GetComponentInChildren<ParticleSystem>().Stop(true, ParticleSystemStopBehavior.StopEmitting);
-                yield return new WaitForSeconds(0.5f);
+                yield return new WaitForSeconds(0.3f);
             }
         }
         else
@@ -42,10 +50,29 @@ public class EgyptController : MonoBehaviour {
             foreach (GameObject t in l_EgyptTorchs)
             {
                 t.GetComponentInChildren<ParticleSystem>().Play(true);
-                yield return new WaitForSeconds(0.3f);
+                yield return new WaitForSeconds(0.2f);
             }
         }
     }
 
-    //EGYPT SPECIAL EFFECTS
+    public IEnumerator Egypt_specialEffect()
+    {
+        foreach (GameObject gO in l_waterfalls)
+        {
+            var l_aux = gO.GetComponentsInChildren<ParticleSystem>();
+            foreach (ParticleSystem p in l_aux)
+                p.Play(true);
+        }
+
+        yield return new WaitForSeconds(5f);
+
+        foreach (GameObject gO in l_waterfalls)
+        {
+            var l_aux = gO.GetComponentsInChildren<ParticleSystem>();
+            foreach (ParticleSystem p in l_aux)
+                p.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+
+        manager.special_effectCorroutine = null;
+    }
 }
